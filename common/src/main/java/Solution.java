740c984ebd2e544a7bd16ed49a4b1d79a4878ea4
malast <malast88@gmail.com>
2018-04-08 21:35:44 +0200

    test coverage improved

--- common/src/main/java/Solution.java
+++ common/src/main/java/Solution.java
@@ -7,11 +7,12 @@ import java.util.Scanner;
  * This particular implementation just reads lines from input and writes them to output reversed.
  * By default uses System.in and System.out but allows to override both for testing purposes.
  */
-class Solution {
-
+final class Solution {
     private static InputStream in;
     private static Scanner scanner;
     private static PrintStream out;
+    private Solution() {
+    }
 
     private static void setupIo() {
         if (in == null) {
@@ -32,7 +33,7 @@ class Solution {
         setupIo();
         boolean isFirstLine = true;
         while (scanner.hasNextLine()) {
-            if (!isFirstLine){
+            if (!isFirstLine) {
                 out.println();
             }
             isFirstLine = false;
